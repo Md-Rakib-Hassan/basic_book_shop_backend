40d654037fecffffc88a3b346622afa788045fdc
Md-Rakib-Hassan <md.rakibhassan4591@gmail.com>
2024-11-22 11:48:03 +0600

    added books interface and model

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
+import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
+
+async function main() {
+  await mongoose.connect(config.database_uri as string)
+}
+main();
 
 app.listen(config.port, () => {
   console.log(`listening on ${config.port}`);
